941b02241f3e90eb61e36562c58ac311697c9491
LeoFdz <lfernandez79@github.com>
2020-02-24 22:16:48 -0600

    minor update on seed file

--- db/seed.sql
+++ db/seed.sql
@@ -10,14 +10,14 @@ VALUES
 INSERT INTO role_table
     (title, salary, department_id)
 VALUES
-    ("Sales Lead", $100000, 1),
-    ("Salesperson", $80000, 1),
-    ("Lead Engineer", $150000, 2),
-    ("Software Engineer", $120000, 2),
-    ("Account Lead", $150000, 3),
-    ("Accountant", $120000, 5),
-    ("Legal Team Lead", $250000, 4),
-    ("Lawyer", $180000, 4);
+    ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Account Lead", 150000, 3),
+    ("Accountant", 120000, 5),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 180000, 4);
 
 INSERT INTO employee_table
     (first_name, last_name, role_id, manager_id)
